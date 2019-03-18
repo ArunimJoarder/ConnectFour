66ad3aa7e511e55f6a7936466aef0369abae03c1
ArunimJoarder <reevu123@gmail.com>
2019-03-18 14:33:22 +0530

    starting integration of AI with UI

--- connect_four.py
+++ connect_four.py
@@ -6,34 +6,36 @@ import random
 
 No_ROW = 6
 No_COLUMN = 7
+DEPTH = 2
 
 def create_board():
     board = numpy.zeros((No_ROW, No_COLUMN))
     return board
 
 def check_column(board, column):
-    return board[0][column] == 0
+    return board[No_ROW - 1][column] == 0
 
 def last_row(board, column):
     for row in range(No_ROW):
-        if board[No_ROW - row - 1][column] == 0:
-            return No_ROW - row - 1
+        if board[row][column] == 0:
+            return row
 
 def drop_coin(board, row, column, coin):
     board[row][column] = coin
+    return row, column
 
 def valid_pos(board, row, column):
-    if row == No_ROW - 1:
+    if row == 0:
         return True
-    elif row < No_ROW - 1:
-        if board[row + 1, column] != 0 and valid_pos(board, row + 1, column):
+    elif row > 0:
+        if board[row - 1, column] != 0 and valid_pos(board, row - 1, column):
             return True
         else:
             return False
                     
 
 def win_cond(board, coin_type):
-    for i in range(No_ROW-1, -1, -1):
+    for i in range(No_ROW):
         for j in range(No_COLUMN):
             try:
                 if board[i][j]  == board[i+1][j] == board[i+2][j] == board[i+3][j] == coin_type:
@@ -92,10 +94,10 @@ def score(board,row, column, coin_type):
 
     # VerticalCheck
     for i in range(1,4):
-        if row + i > No_ROW - 1:
+        if row - i < 0:
             break
         
-        if board[row + i][column] == coin_type:
+        if board[row - i][column] == coin_type:
             count_ver += 1
         else:
             break
@@ -103,12 +105,36 @@ def score(board,row, column, coin_type):
 
     # DiagonalLeftCheck
     for i in range(1,4):
+        if row + i > No_ROW - 1 or column - i < 0:
+            break
+        
+        if valid_pos(board, row + i, column - i):
+            if board[row + i][column - i] == coin_type:  
+                count_diag_l += 1
+            else:
+                break
+        else:
+            break
+
+    for i in range(1,4):
+        if column + i > No_COLUMN - 1 or row - i < 0:
+            break
+        
+        if valid_pos(board, row - i, column + i):
+            if board[row - i][column + i] == coin_type:
+                count_diag_l += 1
+            else:
+                break
+    ##
+
+    # DiagonalRightCheck
+    for i in range(1,4):
         if row - i < 0 or column - i < 0:
             break
         
         if valid_pos(board, row - i, column - i):
             if board[row - i][column - i] == coin_type:  
-                count_diag_l += 1
+                count_diag_r += 1
             else:
                 break
         else:
@@ -120,30 +146,6 @@ def score(board,row, column, coin_type):
         
         if valid_pos(board, row + i, column + i):
             if board[row + i][column + i] == coin_type:
-                count_diag_l += 1
-            else:
-                break
-    ##
-
-    # DiagonalRightCheck
-    for i in range(1,4):
-        if row + i < 0 or column - i < 0:
-            break
-        
-        if valid_pos(board, row + i, column - i):
-            if board[row + i][column - i] == coin_type:  
-                count_diag_r += 1
-            else:
-                break
-        else:
-            break
-
-    for i in range(1,4):
-        if column + i > No_COLUMN - 1 or row - i > No_ROW - 1:
-            break
-        
-        if valid_pos(board, row - i, column + i):
-            if board[row - i][column + i] == coin_type:
                 count_diag_r += 1
             else:
                 break
@@ -155,92 +157,115 @@ def score(board,row, column, coin_type):
     score_r = 10**count_diag_r - (not count_diag_r)
     return max(max(score_h, score_l), max(score_r, score_v))
 
-# Main Game Code
-turn = 1
-
-board = create_board()
-
-game_over = False
-
-vsComputer = int(input("VS: Computer(1) / Player(2) ? : " ))
-print(board)
-print("\n")
-
-while not game_over:
-
-    # Player1 Turn
-    if turn == 1:
-        column = int(input("Player1 input selected column (1 - 7): ")) - 1
-
-        if column > No_COLUMN - 1 or column < 0:
-            print("Enter Valid Column")
-            continue
-
-        if check_column(board, column):
-            row = last_row(board, column)
-            drop_coin(board, row, column, turn)
-
-            game_over = win_cond(board, turn)
-            if game_over:
-                print("\n\nGAME OVER\nPlayer1 Wins!!\n\n")
-        
-            turn = 2
-        else:
-            print("\n\nColumn is filled!\n\n")
-    ##
-
-    # Player2 Turn
-    elif turn == 2 and vsComputer == 2:
-        column = int(input("Player2 input selected column (1 - 7): ")) - 1
-
-        if column > No_COLUMN - 1 or column < 0:
-            print("Enter Valid Column")
-            continue
-
-        if check_column(board, column):
-            row = last_row(board, column)
-            drop_coin(board, row, column, turn)
-
-            game_over = win_cond(board, turn)
-            if game_over:
-                print("\n\nGAME OVER\nPlayer2 Wins!!\n\n")
-            
-            turn = 1
-        else:
-            print("\n\nColumn is filled!\n\n")
-    ##
-
-    # AI Turn
-    else:
-        board_test = deepcopy(board)
-        scores = numpy.zeros(No_COLUMN)
-        scores_1 = numpy.zeros(No_COLUMN)
-        scores_2 = numpy.zeros(No_COLUMN)
+def boardScore(scoreBoard):
+    return numpy.amax(numpy.array(scoreBoard))
+def miniMax(board, no_iter, turn):
+    if no_iter == DEPTH:
+        boardScores = numpy.zeros(No_COLUMN)
         for i in range(No_COLUMN):
+            scores = numpy.zeros(No_COLUMN)
+            board_test = deepcopy(board)
             row_i = last_row(board_test, i)
-            scores_2[i] = score(board_test, row_i, i, 2)
-            scores_1[i] = score(board_test, row_i, i, 1)
+            drop_coin(board_test, row_i, i, turn)
+            scores = score(board_test, row_i, i, 1) + score(board_test, row_i, i, 2)
+            boardScores[i] = ((-1)**no_iter)*boardScore(scores)
+        
+        return boardScores
 
-        scores = scores_2 + scores_1
-        print(scores, "\n")
-        max_score = numpy.amax(numpy.array(scores))
+    else:
+        print("Working on it!")
+            
 
-        print(max_score, "\n")
 
-        ind, = numpy.where(numpy.array(scores) == max_score)
+def print_board(board):
+    print(numpy.flip(board, 0))
 
-        print(ind, "\n")
+# # Main Game Code
+# turn = 1
 
-        column = random.choice(ind)
-        row = last_row(board, column)
+# board = create_board()
 
-        drop_coin(board, row, column, turn)
+# game_over = False
 
-        game_over = win_cond(board, turn)
-        if game_over:
-            print("\n\nGAME OVER\nCOMPUTER Wins!!\n\n")
+# vsComputer = int(input("VS: Computer(1) / Player(2) ? : " ))
+# print_board(board)
+# print("\n")
 
-        turn = 1
-    ##
-    print(board)
-    print("\n")
+# while not game_over:
+
+#     # Player1 Turn
+#     if turn == 1:
+#         column = int(input("Player1 input selected column (1 - 7): ")) - 1
+
+#         if column > No_COLUMN - 1 or column < 0:
+#             print("Enter Valid Column")
+#             continue
+
+#         if check_column(board, column):
+#             row = last_row(board, column)
+#             drop_coin(board, row, column, turn)
+
+#             game_over = win_cond(board, turn)
+#             if game_over:
+#                 print("\n\nGAME OVER\nPlayer1 Wins!!\n\n")
+        
+#             turn = 2
+#         else:
+#             print("\n\nColumn is filled!\n\n")
+#     ##
+
+#     # Player2 Turn
+#     elif turn == 2 and vsComputer == 2:
+#         column = int(input("Player2 input selected column (1 - 7): ")) - 1
+
+#         if column > No_COLUMN - 1 or column < 0:
+#             print("Enter Valid Column")
+#             continue
+
+#         if check_column(board, column):
+#             row = last_row(board, column)
+#             drop_coin(board, row, column, turn)
+
+#             game_over = win_cond(board, turn)
+#             if game_over:
+#                 print("\n\nGAME OVER\nPlayer2 Wins!!\n\n")
+            
+#             turn = 1
+#         else:
+#             print("\n\nColumn is filled!\n\n")
+#     ##
+
+#     # AI Turn
+#     else:
+#         board_test = deepcopy(board)
+#         scores = numpy.zeros(No_COLUMN)
+#         scores_1 = numpy.zeros(No_COLUMN)
+#         scores_2 = numpy.zeros(No_COLUMN)
+#         for i in range(No_COLUMN):
+#             row_i = last_row(board_test, i)
+#             scores_2[i] = score(board_test, row_i, i, 2)
+#             scores_1[i] = score(board_test, row_i, i, 1)
+
+#         scores = scores_2 + scores_1
+#         print(scores, "\n")
+#         max_score = numpy.amax(numpy.array(scores))
+
+#         print(max_score, "\n")
+
+#         ind, = numpy.where(numpy.array(scores) == max_score)
+
+#         print(ind, "\n")
+
+#         column = random.choice(ind)
+#         row = last_row(board, column)
+
+#         drop_coin(board, row, column, turn)
+
+#         game_over = win_cond(board, turn)
+#         if game_over:
+#             print("\n\nGAME OVER\nCOMPUTER Wins!!\n\n")
+
+#         turn = 1
+#     ##
+#     print_board(board)
+#     print("\n")
--- connect_four_GUI.py
+++ connect_four_GUI.py
@@ -15,46 +15,121 @@ coinRadius = 40
 boardWidth = COLUMNS*2*coinRadius + gapSize*(COLUMNS + 1)
 boardHeight = ROWS*2*coinRadius + gapSize*(ROWS + 1)
 
-NAVYBLUE = (60, 60, 100)
+NAVYBLUE = (10, 20, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 
-startWin = pygame.display.set_mode((screenWidth,screenHeight))
-pygame.display.set_caption("ConnectFour")
 
 class Coin:
     def __init__(self, x, y, colour):
-        x = self.x
-        y = self.y
-        colour = self.colour
+        self.x = x
+        self.y = y
+        self.colour = colour
 
     def dropGameCoin (self, win):
-        pygame.draw.circle(win, self.colour, (self.x, self.y), coinRadius)
+        pygame.draw.circle(win, self.colour, (int(self.x), int(self.y)), coinRadius)
 
 class Board:
     def __init__(self, x, y, width, height, colour):
-        x = self.x
-        y = self.y
-        width = self.width
-        height = self.height
-        colour = self.colour
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
 
     def drawBoard(self, win):
         pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.height))
 
+def startupDraw(win, board):
+    board.drawBoard(win)
+    for i in range(7):
+        for j in range(6):
+            circle = Coin((screenWidth - boardWidth)/2 + gapSize + coinRadius + i*(2*coinRadius + gapSize), (screenHeight - boardHeight)*2/3 + gapSize + coinRadius + j*(2*coinRadius + gapSize), BLACK)
+            circle.dropGameCoin(win)
 
+def convert_coordinates(i, j):
+    return (screenHeight - boardHeight)*2/3 + gapSize + coinRadius + 5*(2*coinRadius + gapSize) - i*(2*coinRadius + gapSize), (screenWidth - boardWidth)/2 + gapSize + coinRadius + j*(2*coinRadius + gapSize)
+
+def getCol(x):
+    if x < (screenWidth - boardWidth)/2:
+        return 0
+    elif x > (screenWidth - boardWidth)/2 + boardWidth:
+        return 6
+    for i in range(7):
+        if x > (screenWidth - boardWidth)/2 + gapSize + coinRadius + i*(2*coinRadius + gapSize) - coinRadius - gapSize/2 and x < (screenWidth - boardWidth)/2 + gapSize + coinRadius + i*(2*coinRadius + gapSize) + coinRadius + gapSize/2:
+            return i
 refBoard = cf.create_board()
 playerColours = [RED, YELLOW]
+gameBoard = Board((screenWidth - boardWidth)/2, (screenHeight - boardHeight)*2/3, boardWidth, boardHeight, NAVYBLUE)
 
-run = True
-while(run):
+vsComputer = 0
+vsComputer = int(input("vs PLAYER(1) / vs COMPUTER(2) ? : ")) - 1
+
+startWin = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption("ConnectFour")
+
+startupDraw(startWin, gameBoard)
+turn = 0
+gameover = False
+while not gameover:
     pygame.time.delay(100)
     
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            gameover = True
 
-    pygame.display.update()
+        if vsComputer:
+            if turn == 0:
+                mouseClick = pygame.mouse.get_pressed()
+                if mouseClick[0] == 1:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    col = getCol(mouse_x)
+                    if cf.check_column(refBoard, col):
+                        row = cf.last_row(refBoard, col)
+                        tempRow, tempCol = cf.drop_coin(refBoard, row, col, turn + 1)
+                        gameRow, gameCol = convert_coordinates(tempRow, tempCol)
+                        coin_1 = Coin(gameCol, gameRow, playerColours[turn])
+                        coin_1.dropGameCoin(startWin)
+                    else:
+                        continue
+
+                    if cf.win_cond(refBoard, turn + 1):
+                        gameover = True
+                        pygame.display.update()
+                        print("PLAYER ", turn + 1, "Wins!!")
+
+                    turn += 1
+                    turn %= 2
+
+            else:
+                print("UnderConstruction!")
+                turn += 1
+                turn %= 2
+
+        else:
+            mouseClick = pygame.mouse.get_pressed()
+            if mouseClick[0] == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                col = getCol(mouse_x)
+                if cf.check_column(refBoard, col):
+                    row = cf.last_row(refBoard, col)
+                    tempRow, tempCol = cf.drop_coin(refBoard, row, col, turn + 1)
+                    gameRow, gameCol = convert_coordinates(tempRow, tempCol)
+                    coin_1 = Coin(gameCol, gameRow, playerColours[turn])
+                    coin_1.dropGameCoin(startWin)
+                else:
+                    continue
+
+                if cf.win_cond(refBoard, turn + 1):
+                    gameover = True
+                    pygame.display.update()
+                    print("PLAYER ", turn + 1, "Wins!!")
+
+                turn += 1
+                turn %= 2
+
 
 pygame.quit()
