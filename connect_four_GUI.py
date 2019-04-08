6ba089f8cf1e684091213d941565ffc4e41401c5
ArunimJoarder <reevu123@gmail.com>
2019-04-09 04:31:38 +0530

    Worked more on UI

--- connect_four_GUI.py
+++ connect_four_GUI.py
@@ -16,12 +16,16 @@ coinRadius = 40
 boardWidth = COLUMNS*2*coinRadius + gapSize*(COLUMNS + 1)
 boardHeight = ROWS*2*coinRadius + gapSize*(ROWS + 1)
 
-bg = pygame.image.load('CFbg.jpg')
+bg1 = pygame.image.load('CFbg.jpg')
+bg2 = pygame.image.load('CFbg1.jpg')
+vsPBut = pygame.image.load('vsPlayer.jpg')
+vsCBut = pygame.image.load('vsComp.jpg')
 
 NAVYBLUE = (10, 20, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
 refBoard = cf.create_board()
 playerColours = [RED, YELLOW]
@@ -33,7 +37,7 @@ class Coin:
         self.colour = colour
 
     def dropGameCoin (self, win):
-        pygame.draw.circle(win, self.colour, (int(self.x), int(self.y)), coinRadius)
+        pygame.draw.circle(win, self.colour, (int(self.x), int(self.y) + 5), coinRadius)
 
 class Board:
     def __init__(self, x, y, width, height, colour):
@@ -47,7 +51,7 @@ class Board:
         pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.height))
 
 def startupDraw(win, board):
-    win.blit(bg, (0,0))
+    win.blit(bg2, (0,0))
     # board.drawBoard(win)
     for i in range(6):
         for j in range(7):
@@ -58,6 +62,7 @@ def startupDraw(win, board):
             # else:
             #     circle = Coin(gameCol, gameRow, BLACK)
             #     circle.dropGameCoin(startWin)
+    pygame.display.update()
 
 def convert_coordinates(i, j):
     return (screenHeight - boardHeight)*2/3 + gapSize + coinRadius + 5*(2*coinRadius + gapSize) - i*(2*coinRadius + gapSize), (screenWidth - boardWidth)/2 + gapSize + coinRadius + j*(2*coinRadius + gapSize)
@@ -77,18 +82,60 @@ def coin_animation(win, x, color, board):
     pygame.draw.circle(win, color, (x,(screenHeight - boardHeight)*2//3 - coinRadius - 5), coinRadius-2)
     pygame.display.update()
 
-    
+def firstWindow(win):
+    win.blit(bg1, (0,0))
+    # pygame.draw.rect(win, YELLOW, (screenWidth / 4 - screenWidth / 6, screenHeight / 2 - screenHeight / 10, screenWidth / 3, screenHeight / 5))
+    # pygame.draw.rect(win, RED, (3*screenWidth / 4 - screenWidth / 6, screenHeight / 2 - screenHeight / 10, screenWidth / 3, screenHeight / 5))
+    win.blit(vsPBut, (screenWidth / 4 - 265//2, screenHeight / 2 - 300))
+    win.blit(vsCBut, (3*screenWidth / 4 - 265//2, screenHeight / 2 - 300))    
+    pygame.display.update()
+def lastWindow(win, winner, vsComp):
+    win.blit(bg1, (0,0))
+    fontObj = pygame.font.Font('freesansbold.ttf', 80)
+    if winner == 1:
+        textSurfaceObj = fontObj.render('Player-1 WINS !!!!', True,WHITE,BLACK)
+    elif winner == 0 and vsComp == 0:
+        textSurfaceObj = fontObj.render('Player-2 WINS !!!!', True,WHITE,BLACK)
+    elif winner == 0 and vsComp == 1:
+        textSurfaceObj = fontObj.render('Computer WINS !!!!', True,WHITE,BLACK)
+    else:
+        textSurfaceObj = fontObj.render('DRAW', True,WHITE,BLACK)
+
+
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (screenWidth/2, screenHeight/2)
+    win.blit(textSurfaceObj, textRectObj)
+    pygame.display.update()
+
+
 refBoard = cf.create_board()
 playerColours = [RED, YELLOW]
 gameBoard = Board((screenWidth - boardWidth)/2, (screenHeight - boardHeight)*2/3, boardWidth, boardHeight, NAVYBLUE)
 
-vsComputer = 0
-vsComputer = int(input("vs PLAYER(1) / vs COMPUTER(2) ? : ")) - 1
+# vsComputer = 0
+# vsComputer = int(input("vs PLAYER(1) / vs COMPUTER(2) ? : ")) - 1
 
 startWin = pygame.display.set_mode((screenWidth,screenHeight))
 pygame.display.set_caption("ConnectFour")
 
+firstWindow(startWin)
 
+vsComputer = -1
+while vsComputer == -1:
+    for event in pygame.event.get():
+        mouseClick = pygame.mouse.get_pressed()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        # print(mouseClick)
+        if mouseClick[0] == 1:
+            
+            if mouse_x < screenWidth / 2 - 20:
+                vsComputer = 0
+            elif mouse_x > screenWidth / 2 + 20:
+                vsComputer = 1
+    # print(1)
+    # pygame.time.delay(500)
+
+# print(vsComp)
 
 startupDraw(startWin, gameBoard)
 turn = 0
@@ -132,6 +179,7 @@ while not gameover:
                     turn %= 2
 
             else:
+                startupDraw(startWin, gameBoard)
                 col = cf.move_selector(refBoard, 6, turn + 1, 'AB')
                 if cf.check_column(refBoard, col):
                     row = cf.last_row(refBoard, col)
@@ -139,6 +187,7 @@ while not gameover:
                     gameRow, gameCol = convert_coordinates(tempRow, tempCol)
                     coin_1 = Coin(gameCol, gameRow - 1, playerColours[turn])
                     coin_1.dropGameCoin(startWin)
+                    coin_animation(startWin, screenWidth // 2, playerColours[turn - 1], gameBoard)
                 else:
                     continue
 
@@ -180,5 +229,13 @@ while not gameover:
         if quitFlag:
             pygame.time.delay(2000)
 
-
+# for event in pygame.event.get():
+#     # mouseClick = pygame.mouse.get_pressed()
+#     print(1)
+#     # while mouseClick[0] == 0:
+#     lastWindow(startWin, turn, vsComputer)
+#     if event == pygame.MOUSEBUTTONUP.:
+#         break
+lastWindow(startWin, turn, vsComputer)
+pygame.time.delay(500)
 pygame.quit()
